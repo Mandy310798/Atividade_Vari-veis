08efc34330c464aa4df9ef21a7e78790e9fefdca
Mandy310798 <amanda_kenshi@hotmail.com>
2023-07-05 23:48:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var alerta = "Página carregada com sucesso!"
+
+alert(alerta);
+
+
+
+function soma() {
+    var valor = document
+        .getElementById("inputValor1")
+        .value;
+    var valor2 = document
+        .getElementById("inputValor2")
+        .value;
+        
+    var soma = valor + valor2;
+    document
+        .getElementById("resultado")
+        console.log = "A soma é: " + soma;
+}
+
